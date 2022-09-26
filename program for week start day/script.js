017b2da54074c7d6e9d71fff3191955cae15e084
mari <sermarajraj35@gmail.com>
2022-09-25 22:19:45 -0800

    js-program

---
+++ program for week start day/script.js
@@ -0,0 +1,11 @@
+let d=23,mon=07,yr=2002;
+date=mon+"/"+d+"/"+yr;
+console.log(date);
+let g_date=new Date(date);
+console.log(g_date);
+fn_day=g_date.getDay(g_date);
+console.log(fn_day);
+out=g_date-((24*60*60*1000)*(fn_day-1));
+console.log(out);
+output=new Date(out);
+console.log(output); 
