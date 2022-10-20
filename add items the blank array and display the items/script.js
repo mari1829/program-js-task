81f2468adc6f85702b5bddefb9dd08b1268cc6e9
mari <sermarajraj35@gmail.com>
2022-10-19 20:46:11 -0800

    javascript-progrm

---
+++ add items the blank array and display the items/script.js
@@ -0,0 +1,35 @@
+let c=document.getElementById("btn"),
+ d=document.getElementById("dis"),
+ e=document.getElementById("inne");
+
+ function myfun(){
+ let ar=[],list,
+ ab=d.value;
+//  ar.push(ab);
+// for(i=0; i<ar.length; i++){
+    list=document.createElement("li");
+e.append(list);
+list.innerHTML=ab;
+    
+
+ }
+
+addEventListener
+// }
+ c.addEventListener("click",myfun)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- program for even number using hifan(-)/script.js
+++ program for even number using hifan(-)/script.js
@@ -1,16 +1,25 @@
-let c=parseInt(prompt("value"));
-let f=[];
+// let c=parseInt(prompt("value"));
+// let f=[];
 
-d=c.toString();
-for(i=0; i<d.length; i++){
-    // console.log(d[i-1]);
-    if(d[i-1]%2==0 && d[i]%2==0){   
-        f.push("-",d[i])
+// d=c.toString();
+// for(i=0; i<d.length; i++){
+//     // console.log(d[i-1]);
+//     if(d[i-1]%2==0 && d[i]%2==0){   
+//         f.push("-",d[i])
+//     }
+//     else{
+//         f.push(d[i])
+//         // console.log(f);
+//     }
+// }
+// f=f.join("");
+// console.log(f);
+
+
+let a=[1,2,3,3,4,4,5,7,8];
+for(i=0; i<a.length; i++){
+    for(j=i+1; j<a.length; i++){
+       
     }
-    else{
-        f.push(d[i])
-        // console.log(f);
-    }
+    console.log(j);
 }
-f=f.join("");
-console.log(f);
