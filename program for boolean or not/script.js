a995201af8b6ef0c160778393a7da09367ad7d11
mari <sermarajraj35@gmail.com>
2022-11-04 08:04:56 -0800

    js-program

---
+++ program for boolean or not/script.js
@@ -0,0 +1,9 @@
+// check the number is binary number or not
+let arr = ["1", "0"];
+for (i = 0; i < arr.length; i++) {
+  if (arr[i] == "0" || arr[i] == "1") {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
